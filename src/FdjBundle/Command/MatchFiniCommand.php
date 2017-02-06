9eee3cd3f4cb1273491df16e42722d47db9d848b
Jpilosel <formation@pilosel.fr>
2017-02-06 15:38:45 +0100

    matchFinicommand ok

--- src/FdjBundle/Command/MatchFiniCommand.php
+++ src/FdjBundle/Command/MatchFiniCommand.php
@@ -29,77 +29,77 @@ class MatchFiniCommand extends ContainerAwareCommand
         $output->writeln(['cote inputt', '============',]);
         $em = $this->getContainer()->get('doctrine')->getManager();
 
-//        $resultats = $em->getRepository('FdjBundle:Formules')->findByOk(1);
+        $resultats = $em->getRepository('FdjBundle:Formules')->findByOk(1);
         $resultats = $em->getRepository('FdjBundle:Formules')->findAll();
-
+        
 
         foreach ($resultats as $resultat) {
-            $resultat->setOk(1);
+            $resultat->setOk(2);
             $em->persist($resultat);
             $em->flush();
 
-//            $eventId = $resultat->getEventId();
+            $eventId = $resultat->getEventId();
+
+            $matchs = $em->getRepository('FdjBundle:Sport')->findByEventId($eventId);
+//            var_dump($matchs);
+            if ($matchs) {
+//                $resultat->setOk(1);
+//                $em->persist($resultat);
+//                $em->flush();
+//                var_dump($matchFini);
+                foreach ($matchs as $match) {
+//                    if ( $match->getOk() == null){
+                        $marketId = $match->getMarketId();
+                        var_dump($marketId);
+                        $matchsFinis = $em->getRepository('FdjBundle:MatchFini')->findByMarketId($marketId);
+
+                        if ($matchsFinis){
+//                            $match->setOk(2);
+//                            $em->persist($match);
+//                            $resultat->setOk(2);
+//                            $em->persist($resultat);
+//                            $em->flush();
+    //                        var_dump($matchsFinis[0]->getMarketTypeGroup());
+    //                        var_dump($matchsFinis[0]->getSportId());
+                        }else {
+    //                    var_dump($resultat->getMarketType());
+    //                    var_dump($match->getMarketType());
+    //                    var_dump($match->getMarketTypeGroup());
+                            if ($match->getMarketTypeGroup() == $resultat->getMarketTypeGroup()) {
+                                $matchFini = new Matchfini();
+                                $matchFini->setEventId($match->getEventId());
+                                $matchFini->setMarketId($match->getMarketId());
+                                $matchFini->setHasCombiBonus($match->getHasCombiBonus());
+                                $matchFini->setSportId($match->getSportId());
+                                $matchFini->setIndexP($match->getIndexP());
+                                $matchFini->setMarketType($match->getMarketType());
+                                $matchFini->setMarketTypeGroup($match->getMarketTypeGroup());
+                                $matchFini->setMarketTypeId($match->getmarketTypeId());
+                                $matchFini->setEnd($match->getEnd());
+                                $matchFini->setLabel($match->getLabel());
+                                $matchFini->setEventType($match->getEventType());
+                                $matchFini->setCompetition($match->getCompetition());
+                                $matchFini->setCompetitionId($match->getCompetitionId());
+                                $matchFini->setNbMarkets($match->getNbMarkets());
+                                $matchFini->setUn($match->getUn());
+                                $matchFini->setNul($match->getNul());
+                                $matchFini->setDeux($match->getDeux());
+                                $matchFini->setResultat($resultat->getResult());
+                                var_dump($matchFini);
+                                $em->persist($matchFini);
+                                $match->setOk(2);
+                                $em->persist($match);
+                                $resultat->setOk(2);
+                                $em->persist($resultat);
+                                $em->flush();
+                            }
+//                        }
+                    }
+                }
 //
-//            $matchs = $em->getRepository('FdjBundle:Sport')->findByEventId($eventId);
-////            var_dump($matchs);
-//            if ($matchs) {
-////                $resultat->setOk(1);
-////                $em->persist($resultat);
-////                $em->flush();
-////                var_dump($matchFini);
-//                foreach ($matchs as $match) {
-////                    if ( $match->getOk() == null){
-//                        $marketId = $match->getMarketId();
-//                        var_dump($marketId);
-//                        $matchsFinis = $em->getRepository('FdjBundle:MatchFini')->findByMarketId($marketId);
-//
-//                        if ($matchsFinis){
-////                            $match->setOk(2);
-////                            $em->persist($match);
-////                            $resultat->setOk(2);
-////                            $em->persist($resultat);
-////                            $em->flush();
-//    //                        var_dump($matchsFinis[0]->getMarketTypeGroup());
-//    //                        var_dump($matchsFinis[0]->getSportId());
-//                        }else {
-//    //                    var_dump($resultat->getMarketType());
-//    //                    var_dump($match->getMarketType());
-//    //                    var_dump($match->getMarketTypeGroup());
-//                            if ($match->getMarketTypeGroup() == $resultat->getMarketTypeGroup()) {
-//                                $matchFini = new Matchfini();
-//                                $matchFini->setEventId($match->getEventId());
-//                                $matchFini->setMarketId($match->getMarketId());
-//                                $matchFini->setHasCombiBonus($match->getHasCombiBonus());
-//                                $matchFini->setSportId($match->getSportId());
-//                                $matchFini->setIndexP($match->getIndexP());
-//                                $matchFini->setMarketType($match->getMarketType());
-//                                $matchFini->setMarketTypeGroup($match->getMarketTypeGroup());
-//                                $matchFini->setMarketTypeId($match->getmarketTypeId());
-//                                $matchFini->setEnd($match->getEnd());
-//                                $matchFini->setLabel($match->getLabel());
-//                                $matchFini->setEventType($match->getEventType());
-//                                $matchFini->setCompetition($match->getCompetition());
-//                                $matchFini->setCompetitionId($match->getCompetitionId());
-//                                $matchFini->setNbMarkets($match->getNbMarkets());
-//                                $matchFini->setUn($match->getUn());
-//                                $matchFini->setNul($match->getNul());
-//                                $matchFini->setDeux($match->getDeux());
-//                                $matchFini->setResultat($resultat->getResult());
-//                                var_dump($matchFini);
-//                                $em->persist($matchFini);
-//                                $match->setOk(2);
-//                                $em->persist($match);
-//                                $resultat->setOk(2);
-//                                $em->persist($resultat);
-//                                $em->flush();
-//                            }
-////                        }
-//                    }
-//                }
-////
-////                var_dump($resultat);
-////                var_dump($match);
-//            }
+//                var_dump($resultat);
+//                var_dump($match);
+            }
         }
         $output->writeln(['============','resultat fin',]);
     }
