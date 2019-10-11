d086e20dff43e78dc2aa867930d82c2a9418a7c6
joel <formation@pilosel.fr>
2019-10-11 15:37:08 +0200

    date de saisie

--- src/FdjBundle/Entity/Formules.php
+++ src/FdjBundle/Entity/Formules.php
@@ -31,7 +31,7 @@ class Formules
     /**
      * @var string
      *
-     * @ORM\Column(name="Ok", type="string", length=255)
+     * @ORM\Column(name="Ok", type="string", length=255, nullable=true)
      */
     private $Ok;
 
